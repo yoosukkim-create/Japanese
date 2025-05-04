aeb5f172e50ff2adaca7c3d33dcfdf2655a6ea4b
yoosuk <hp920113@gmail.com>
2025-05-04 21:21:52 +0900

    refact: theme provider

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
 
 class ThemeProvider extends ChangeNotifier {
+  // 상태 변수들
   bool _isDarkMode = true;
-  Color _mainColor = const Color(0xFFFFB7C5); // 기본값: 체리 블러썸
-  bool _showLastViewedTime = true;  // 마지막으로 본 시간 표시 여부
+  bool _showLastViewedTime = true;
+  bool _showMemoryParams = true;
+  Color _mainColor = const Color(0xFFFFB7C5); // 기본 색상: 체리 블러썸
 
+  // 정적 색상 정의
   static const Color navyBlue = Color(0xFF4A5568);
   static const Color cherryBlossom = Color(0xFFFFB7C5);
 
+  // Getter
   bool get isDarkMode => _isDarkMode;
-  Color get mainColor => _mainColor;
   bool get showLastViewedTime => _showLastViewedTime;
-
-  bool _showMemoryParams = true;
   bool get showMemoryParams => _showMemoryParams;
-  
+  Color get mainColor => _mainColor;
+
+  ThemeData get themeData => ThemeData(
+        brightness: _isDarkMode ? Brightness.dark : Brightness.light,
+        primaryColor: _mainColor,
+      );
+
+  // Setter 및 토글 함수들
   void toggleTheme() {
     _isDarkMode = !_isDarkMode;
     notifyListeners();
@@ -33,12 +41,5 @@ class ThemeProvider extends ChangeNotifier {
   void toggleMemoryParams() {
     _showMemoryParams = !_showMemoryParams;
     notifyListeners();
-  }
-
-  ThemeData get themeData {
-    return ThemeData(
-      brightness: _isDarkMode ? Brightness.dark : Brightness.light,
-      primaryColor: _mainColor,
-    );
   }
 } 
