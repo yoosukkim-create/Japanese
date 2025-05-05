b7b7d02ecc613ff53054fe39c48edfef90dfd15d
yoosuk <hp920113@gmail.com>
2025-05-05 10:27:34 +0900

    refact: delete home screen file

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import 'package:japanese/providers/theme_provider.dart';
 import 'package:japanese/providers/study_provider.dart';
 import 'package:japanese/services/japanese_data_service.dart';
 import 'package:japanese/theme/app_theme.dart';
-import 'package:japanese/views/screens/home_screen.dart';
 import 'package:japanese/views/screens/word_book_screen.dart';
 import 'package:japanese/views/screens/word_list_screen.dart';
 import 'package:japanese/views/screens/word_group_screen.dart';
