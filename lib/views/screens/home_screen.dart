b5c868b10a87d0f2511abc911bf1af1a7b53bd5a
yoosuk <hp920113@gmail.com>
2025-05-29 23:26:13 +0900

    Refactoring home screen #2

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -5,6 +5,8 @@ import 'package:japanese/viewmodels/home_viewmodel.dart';
 import 'package:japanese/providers/theme_provider.dart';
 import 'package:japanese/providers/study_provider.dart';
 
+import 'package:japanese/models/word_book.dart';
+
 import 'package:japanese/views/screens/word_list_screen.dart';
 import 'package:japanese/views/screens/settings_screen.dart';
 
@@ -29,6 +31,138 @@ class _HomeScreenState extends State<HomeScreen> {
     Provider.of<StudyProvider>(context, listen: false).loadRecentLists();
   }
 
+  Widget _buildLoadingView() {
+    return const Scaffold(body: Center(child: CircularProgressIndicator()));
+  }
+
+  Widget _buildErrorView(HomeViewModel homeVM) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('오류: ${homeVM.error}'),
+            ElevatedButton(
+              onPressed: homeVM.loadData,
+              child: const Text('다시 시도'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  PreferredSizeWidget _buildAppBar(
+    BuildContext context,
+    ThemeProvider themeProvider,
+  ) {
+    return AppBar(
+      title: Align(
+        alignment: ThemeProvider.globalBarAlignment,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/memory_transparent.png',
+              width: ThemeProvider.mainIconImage(context),
+              height: ThemeProvider.mainIconImage(context),
+            ),
+            ThemeProvider.gap8,
+            Text(
+              ThemeProvider.globalTitle,
+              style: ThemeProvider.globalBarStyle(
+                context,
+              ).copyWith(color: themeProvider.mainColor),
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.settings),
+          onPressed:
+              () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SettingsScreen()),
+              ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSearchBar(
+    BuildContext context,
+    ThemeProvider themeProvider,
+    HomeViewModel homeVM,
+  ) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        controller: homeVM.searchController,
+        onChanged:
+            (query) => homeVM.searchWords(
+              query,
+              (results) => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder:
+                      (_) => WordListScreen(
+                        title: '"$query" 검색 결과',
+                        words: results,
+                      ),
+                ),
+              ),
+              (error) {
+                ScaffoldMessenger.of(
+                  context,
+                ).showSnackBar(SnackBar(content: Text(error)));
+              },
+            ),
+        decoration: InputDecoration(
+          hintText: '단어 검색...',
+          prefixIcon: const Icon(Icons.search),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(
+              ThemeProvider.globalCornerRadius,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(
+              ThemeProvider.globalCornerRadius,
+            ),
+            borderSide: BorderSide(color: themeProvider.mainColor, width: 2),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWordbookList(
+    ThemeProvider themeProvider,
+    StudyProvider studyProvider,
+    List<Wordbook> wordbooks,
+  ) {
+    return ListView(
+      padding: const EdgeInsets.only(bottom: 24.0),
+      children: [
+        RecentWordBookCard(
+          studyProvider: studyProvider,
+          themeProvider: themeProvider,
+          onTap: _navigateToWordList,
+        ),
+        BasicWordBookCard(
+          wordbooks: wordbooks,
+          themeProvider: themeProvider,
+          isDarkMode: isDarkMode,
+        ),
+        CustomWordbookCard(
+          themeProvider: themeProvider,
+          isDarkMode: isDarkMode,
+        ),
+      ],
+    );
+  }
+
   void _navigateToWordList(Map<String, dynamic> wordList) {
     Navigator.push(
       context,
@@ -46,134 +180,20 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Consumer3<HomeViewModel, ThemeProvider, StudyProvider>(
       builder: (context, homeVM, themeProvider, studyProvider, _) {
-        if (homeVM.isLoading) {
-          return const Scaffold(
-            body: Center(child: CircularProgressIndicator()),
-          );
-        }
-
-        if (homeVM.error.isNotEmpty) {
-          return Scaffold(
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('오류: ${homeVM.error}'),
-                  ElevatedButton(
-                    onPressed: homeVM.loadData,
-                    child: const Text('다시 시도'),
-                  ),
-                ],
-              ),
-            ),
-          );
-        }
+        if (homeVM.isLoading) return _buildLoadingView();
+        if (homeVM.error.isNotEmpty) return _buildErrorView(homeVM);
 
         return Scaffold(
-          appBar: AppBar(
-            title: Align(
-              alignment: ThemeProvider.globalBarAlignment,
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.asset(
-                    'assets/images/memory_transparent.png',
-                    width: ThemeProvider.mainIconImage(context),
-                    height: ThemeProvider.mainIconImage(context),
-                  ),
-                  ThemeProvider.gap8,
-                  Text(
-                    ThemeProvider.globalTitle,
-                    style: ThemeProvider.globalBarStyle(
-                      context,
-                    ).copyWith(color: themeProvider.mainColor),
-                  ),
-                ],
-              ),
-            ),
-            actions: [
-              IconButton(
-                icon: const Icon(Icons.settings),
-                onPressed:
-                    () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const SettingsScreen(),
-                      ),
-                    ),
-              ),
-            ],
-          ),
+          appBar: _buildAppBar(context, themeProvider),
           body: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // 🔍 고정된 검색창
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextField(
-                  controller: homeVM.searchController,
-                  onChanged:
-                      (query) => homeVM.searchWords(
-                        query,
-                        (results) {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder:
-                                  (_) => WordListScreen(
-                                    title: '"$query" 검색 결과',
-                                    words: results,
-                                  ),
-                            ),
-                          );
-                        },
-                        (error) {
-                          ScaffoldMessenger.of(
-                            context,
-                          ).showSnackBar(SnackBar(content: Text(error)));
-                        },
-                      ),
-                  decoration: InputDecoration(
-                    hintText: '단어 검색...',
-                    prefixIcon: const Icon(Icons.search),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(
-                        ThemeProvider.globalCornerRadius,
-                      ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(
-                        ThemeProvider.globalCornerRadius,
-                      ),
-                      borderSide: BorderSide(
-                        color: themeProvider.mainColor,
-                        width: 2,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-
-              // 📜 나머지 리스트는 스크롤 가능하게
+              _buildSearchBar(context, themeProvider, homeVM),
               Expanded(
-                child: ListView(
-                  padding: const EdgeInsets.only(bottom: 24.0),
-                  children: [
-                    RecentWordBookCard(
-                      studyProvider: studyProvider,
-                      themeProvider: themeProvider,
-                      onTap: _navigateToWordList,
-                    ),
-                    BasicWordBookCard(
-                      wordbooks: homeVM.wordbooks,
-                      themeProvider: themeProvider,
-                      isDarkMode: isDarkMode,
-                    ),
-                    CustomWordbookCard(
-                      themeProvider: themeProvider,
-                      isDarkMode: isDarkMode,
-                    ),
-                  ],
+                child: _buildWordbookList(
+                  themeProvider,
+                  studyProvider,
+                  homeVM.wordbooks,
                 ),
               ),
             ],
